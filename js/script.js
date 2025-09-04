cf48a134a8c7e1a390a44e7d326bbb9e81ea3ff4
arpit-builds <arpitprakash86@gmail.com>
2025-09-04 21:25:02 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function SecondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`https://spotify-clone-beta-umber.vercel.app/${folder}/`);
+    let a = await fetch(`http://192.168.1.9:3000/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response
@@ -70,7 +70,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`https://spotify-clone-beta-umber.vercel.app/songs/`);
+    let a = await fetch(`http://192.168.1.9:3000/songs/`);
     let response = await a.text();
     let div = document.createElement("div")
     let cardContainer = document.querySelector(".cardContainer")
@@ -82,7 +82,7 @@ async function displayAlbums() {
         const e = array[index];
         if (e.href.includes("/songs")) {
             let folder = (e.href.split("/").slice(-2)[0])
-            let a = await fetch(`https://spotify-clone-beta-umber.vercel.app/songs/${folder}/info.json`)
+            let a = await fetch(`http://192.168.1.9:3000/${folder}/info.json`)
             let response = await a.json()
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
                         <div class="play">
